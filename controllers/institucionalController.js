8503a186e423a391cdc7e6a2aa051cec069b4450
Henkoff <henrique.f.ibiapino@gmail.com>
2021-05-24 20:56:43 -0300

    Front e rotas

---
+++ controllers/institucionalController.js
@@ -0,0 +1,21 @@
+  
+const institucionalController = {
+    index: (request, response) => {
+        return response.render('index', { titulo: 'Home' });
+    },
+    sobre: (request, response) => {
+        return response.render('sobre', { titulo: 'Sobre' });
+    },
+    servicos: (request, response) => {
+        return response.render('servicos', { titulo: 'Serviços' });
+    },
+    contato: (request, response) => {
+        return response.render('contato', { titulo: 'Contato' });
+    }
+}
+
+module.exports = institucionalController;
+
+
+//response é a resposta para o usuario
+// o parametro é qual view vc quer renderizar tipo 'index', 'sobre', 'servicos' e etc...
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const router = express.Router();
+const petsController = require('../controllers/petsController');
+const institucionalController = require('../controllers/institucionalController');
+
+// rotas para paginas institucionais:
+// http://localhost:3000/
+router.get('/', institucionalController.index);
+
+// http://localhost:3000/sobre
+router.get('/sobre', institucionalController.sobre);
+
+// http://localhost:3000/servicos
+router.get('/servicos', institucionalController.servicos);
+
+// http://localhost:3000/contato
+router.get('/contato', institucionalController.contato);
+
+
+// rota para /pets que retorna o método index da petsController
+router.get('/pets', petsController.index);
+
+router.get('/pets/:nome', petsController.show);
+
+module.exports = router;
